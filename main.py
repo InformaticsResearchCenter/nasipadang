ccf4c9c838fc35a1f3f95b925b1e93c571d667ca
trianggadios <trianggadio@gmail.com>
2019-10-04 22:01:30 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import nasiPadang
+
+nasipad = nasiPadang.NasiPadang()
+
+npm = '1184047'
+password = 'isSAME10'
+
+wekwek = nasipad.getAllNilai(npm, password)
+
+print(wekwek)
+
+waduwek=nasipad.getNilai(wekwek)
+
+print(waduwek)
+
+adaw = nasipad.getIndeks(waduwek)
+
+print(adaw)
